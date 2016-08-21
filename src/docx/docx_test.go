ebe38199a15cf5d71a9b770ef25cedd845e26792
Stefan Radulian <Stefan.Radulian@brainloop.com>
2016-08-21 16:20:39 +0200

    Moved concern of writing file into the test

--- src/docx/docx_test.go
+++ src/docx/docx_test.go
@@ -1,9 +1,12 @@
 package docx_test
 
 import (
+	"bufio"
 	"docx"
 	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestReplace(t *testing.T) {
@@ -33,7 +36,13 @@ func TestReplace(t *testing.T) {
 
 	docx1.Replace("AgendaHeader", "My Cool Agenda", 1)
 	docx1.Replace("MeetingDate", "1.1.2017", 1)
-
 	docx1.ReplaceLoop("topic", topics)
-	docx1.WriteToFile("output.docx")
+
+	f, err := os.Create("output-" + time.Now().Format("20060102150405") + ".docx")
+	if err != nil {
+		panic(err)
+	}
+	w := bufio.NewWriter(f)
+	docx1.Write(w)
+
 }
